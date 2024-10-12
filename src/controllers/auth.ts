fd2edd109fa8b47c0aceb7d57bdf4c7dda4dcb5a
Galib-23 <aagalib2323@gmail.com>
2024-10-12 17:17:30 +0600

    creaet login route and controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,20 +1,19 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
-import { hashSync } from "bcrypt";
+import { compareSync, hashSync } from "bcrypt";
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "../../secrets";
 
 export const signup = async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
-
   let user = await prismaClient.user.findFirst({
     where: {
       email: email,
     },
   });
-
   if (user) {
     throw Error("User already exists");
   }
-
   user = await prismaClient.user.create({
     data: {
       name,
@@ -22,7 +21,27 @@ export const signup = async (req: Request, res: Response) => {
       password: hashSync(password, 10),
     }
   })
-
   res.json(user);
+};
+
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  let user = await prismaClient.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+  if (!user) {
+    throw Error("User doesn't exists");
+  }
+  if (!compareSync(password, user.password)) {
+    throw Error("Wrong Credentials");
+  }
+  const token = jwt.sign({
+    userId: user.id
+  }, JWT_SECRET);
+
+  res.json({user, token});
 
 };
