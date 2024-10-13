97614d99baa6cd3699b8289be33ad790b60c9022
Galib-23 <aagalib2323@gmail.com>
2024-10-13 22:12:53 +0600

    update product route created

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
 
 export const createProduct = async (req: Request, res: Response) => {
   const product = await prismaClient.product.create({
@@ -9,4 +11,34 @@ export const createProduct = async (req: Request, res: Response) => {
     }
   });
   res.json(product);
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const product = req.body;
+    if (product.tags) {
+      product.tags = product.tags.join(',');
+    }
+    const updatedProduct = await prismaClient.product.update({
+      where: {
+        id: parseInt(req.params.id)
+      },
+      data: product,
+    })
+    res.json(updatedProduct);
+  } catch (error) {
+    throw new NotFoundException("Product Not Found", ErrorCode.PRODUCT_NOT_FOUND);
+  }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  
+}
+
+export const listProducts = async (req: Request, res: Response) => {
+  
+}
+
+export const getProductById = async (req: Request, res: Response) => {
+  
 }
