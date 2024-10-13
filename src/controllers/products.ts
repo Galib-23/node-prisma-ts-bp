f13a763dd80a81c90a7527509074088df1751583
Galib-23 <aagalib2323@gmail.com>
2024-10-13 22:29:44 +0600

    complete all product controllers

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -7,38 +7,77 @@ export const createProduct = async (req: Request, res: Response) => {
   const product = await prismaClient.product.create({
     data: {
       ...req.body,
-      tags: req.body.tags.join(',')
-    }
+      tags: req.body.tags.join(","),
+    },
   });
   res.json(product);
-}
+};
+
 
 export const updateProduct = async (req: Request, res: Response) => {
   try {
     const product = req.body;
     if (product.tags) {
-      product.tags = product.tags.join(',');
+      product.tags = product.tags.join(",");
     }
     const updatedProduct = await prismaClient.product.update({
       where: {
-        id: parseInt(req.params.id)
+        id: parseInt(req.params.id),
       },
       data: product,
-    })
+    });
     res.json(updatedProduct);
   } catch (error) {
-    throw new NotFoundException("Product Not Found", ErrorCode.PRODUCT_NOT_FOUND);
+    throw new NotFoundException(
+      "Product Not Found",
+      ErrorCode.PRODUCT_NOT_FOUND,
+    );
   }
-}
+};
+
 
 export const deleteProduct = async (req: Request, res: Response) => {
-  
-}
+  try {
+    await prismaClient.product.delete({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    throw new NotFoundException(
+      "Product not found",
+      ErrorCode.PRODUCT_NOT_FOUND,
+    );
+  }
+};
+
 
 export const listProducts = async (req: Request, res: Response) => {
-  
-}
+  const count = await prismaClient.product.count();
+  const products = await prismaClient.product.findMany({
+    skip: +req.query.skip! || 0,
+    take: 5
+  });
+  res.json({
+    count, data: products
+  })
+};
+
 
 export const getProductById = async (req: Request, res: Response) => {
-  
-}
+  try {
+    const productId = parseInt(req.params.id);
+    const product = await prismaClient.product.findFirst({
+      where: {
+        id: productId
+      }
+    });
+    res.json(product);
+  } catch (error) {
+    throw new NotFoundException(
+      "Product not found",
+      ErrorCode.PRODUCT_NOT_FOUND,
+    );
+  }
+};
